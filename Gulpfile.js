24655a46adf543d98c88f7dedf3b889f1b93ba34
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-07-11 01:47:30 -0400

    added scripts task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,14 @@
 var gulp    = require('gulp')
+    uglify  = require('gulp-uglify')
     concat  = require('gulp-concat')
-    minify  = require('gulp-minify');
+    minify  = require('gulp-minify-css');
+
+gulp.task('scripts', function() {
+    return gulp.src('assets/js/*.js')
+        .pipe(concat('bundle.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js'));
+});
 
 gulp.task('default', [
     'scripts',
